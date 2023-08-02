899bdc7e425b00e47b8a44c7808a4c72714f6c24
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-08-02 23:08:58 +0500

    task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напиши программу, которая 
+// на вход принимает два числа и 
+// проверяет, является ли 
+// первое число квадратом второго
+// а=25 б=5 -> да, а=2 б=10 -> нет
+
+Console.WriteLine ("Введите два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
