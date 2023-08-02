b15fbf7f242c19db7550b1172b7440b89f31aef0
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-08-02 22:46:20 +0500

    task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. на вход подает число 
+// 2. и выдает его квадрат
+// Например 4->16, -3->9, -7->49
+
+Console.WriteLine ("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int squere = number * number;
+Console.WriteLine ($"Квадрат числа {number} = {squere}");
