e516a89f30febad714f2e6430db99e12dff2190e
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-08-02 23:42:50 +0500

    task08

--- task08/Program.cs
+++ task08/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//На вход подается число N, на выходе все четные числа от 1 до N
+
+Console.WriteLine ("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (i < N);
+{
+    i += 2;
+    Console.WriteLine (i);
+}
