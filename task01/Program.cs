68d1341b366a64c4d1e01f5bb62266ca37614066
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-08-12 13:18:53 +0500

    task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -7,3 +7,13 @@
 Console.WriteLine ("Введите два числа");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
+{
+    if(a==b*b)
+    {
+        Console.WriteLine ("да");
+    }
+    else 
+    {
+        Console.WriteLine("нет");
+    }
+}
--- task08/Program.cs
+++ task08/Program.cs
@@ -1,11 +1,10 @@
 ﻿//На вход подается число N, на выходе все четные числа от 1 до N
-// ошибка в синтаксисе
 
 Console.WriteLine ("Введите число N");
 int N = Convert.ToInt32(Console.ReadLine());
 int i = 0;
-while (i < N);
+while(i < N);
 {
     i += 2;
-    Console.WriteLine (i);
+    Console.WriteLine(i);
 }
