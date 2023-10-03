1e21e5883794a588a4d69bf2614b4e1e45066ca2
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-10-03 22:12:34 +0500

    task49

---
+++ task49/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 49: Задайте двумерный массив. 
+//Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) 
+{
+    //                        0       1
+    int[,] matrix = new int[rows, columns]; // rows = 3, columns = 4
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 4
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        //Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            Console.Write($"{matrix[i, j], 5}");
+        }
+       //Console.WriteLine("  |");
+       Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
