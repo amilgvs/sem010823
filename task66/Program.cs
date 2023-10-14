981289f28b3446b0fa562885083788f7bbc3af34
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-10-14 08:33:55 +0500

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// /*Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30*/
+
+Console.WriteLine("Задайте значение M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Count(int M, int N)
+{
+    if (M == N) return M;
+    else if (M < N) return M + Count (M+1,N);
+	else return M + Count(M-1,N);
+}
+Console.WriteLine($"Сумма чисел от М до N = {Count(M , N)}");
+
